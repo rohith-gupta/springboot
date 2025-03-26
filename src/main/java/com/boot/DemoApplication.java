9a01653661a60b6484e270faa094a550860fb824
rohith-gupta <rmarri@my.bridgeport.edu>
2025-03-26 19:58:07 -0400

    creating project using springboot and saving data using data jpa

---
+++ src/main/java/com/boot/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.boot.dao.UserRepo;
+import com.boot.entities.Users;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		
+		UserRepo userrepo = (UserRepo) context.getBean("userRepo");
+		
+		Users user = new Users();
+		
+		user.setName("rohith");
+		user.setPassword("qwerty");
+		
+		userrepo.save(user);
+		
+		System.out.print(user);
+	}
+
+}
